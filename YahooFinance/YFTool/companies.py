53aa29ce49478b26439ddf118865feae336077f8
iMoeNya <sym9688@outlook.com>
2018-09-11 21:48:07 +0800

    Add parser

--- YahooFinance/YFTool/companies.py
+++ YahooFinance/YFTool/companies.py
@@ -1,5 +1,5 @@
 import requests
-from bs4 import BeautifulSoup
+from . import parser
 
 from enum import Enum
 
@@ -14,16 +14,8 @@ def get_profile_of(ticker):
     profile_url = _url_for_ticker(ticker, DataType.profile)
 
     # send request, convert response to Soup object
-    profile_soup = _soup_from_URL(profile_url)
-
-    # narrow down to profile section
-    profile_section = profile_soup.find('div', class_='qsp-2col-profile')
-
-    # get data we are interested in
-    company_name = profile_section.h3.string
-    sector, industry, nb_employees = [tag.string for tag in profile_soup.find_all('strong')]
-    return company_name, sector, industry, nb_employees
-
+    doc = _html_doc_from_URL(profile_url)
+    return parser.parse_profile(doc)
 
 
 def get_statistics_of(ticker):
@@ -32,15 +24,10 @@ def get_statistics_of(ticker):
     statistics_URL = _url_for_ticker(ticker, DataType.statistics)
 
     # send request, convert response to Soup object
-    statistics_soup = _soup_from_URL(statistics_URL)
+    doc = _html_doc_from_URL(statistics_URL)
+    return parser.parse_statistics(doc)
 
-    # narrow down to statistics section
-    statistics_section = statistics_soup.find('section', attrs={ 'data-test' : 'qsp-statistics' })
 
-    # get Market Cap (intraday) and 52-Week Change from section
-    market_cap = statistics_soup.find(string='Market Cap (intraday)').parent.parent.next_sibling.string
-    fifty_two_week_change = statistics_soup.find(string='52-Week Change').parent.parent.next_sibling.string
-    return market_cap, fifty_two_week_change
 
 
 def _url_for_ticker(ticker, data_type):
@@ -49,5 +36,5 @@ def _url_for_ticker(ticker, data_type):
     else:  # if data_type == DataType.statistics
         return 'https://finance.yahoo.com/quote/{}/key-statistics'.format(ticker)
 
-def _soup_from_URL(url):
-    return BeautifulSoup(requests.get(url).text)
+def _html_doc_from_URL(url):
+    return requests.get(url).text
